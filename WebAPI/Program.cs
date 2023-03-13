b85ae86f75fe17f20d0f7c00a536b8ef0410075e
Gültekin <gultekinyilmaz@hotmail.com.tr>
2023-03-13 18:25:14 +0300

    11.Gün Ders Sonu

---
+++ WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Autodac,Ninjet,CastleWindsor,StrucureMap,LightInject,DryInject --> IoC Container
+//AOP
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IProductService,ProductManager>(); // Tüm bellekte 1 manager oluþturuyor ve hepis bu referansý görüyor.Bu heryerde yapýlan kullaýnlan bir teknik olup api ye özel deðildir.
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
